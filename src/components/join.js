e5adecc255ae52fc718e3c9cd4cb6e72db51a443
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2021-06-03 10:46:58 -0300

    feita inclusão de itens do join e estilização inicial, precisa de posicionamento final

--- src/components/join.js
+++ src/components/join.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import joinImg from "../assets/images/bloco_final_image.svg";
 
 const Join = () => {
   return (
     <>
-      <JoinContent>Join</JoinContent>
+      <JoinContent>
+        <JoinTitle>Join our membership to get special offer</JoinTitle>
+        <Input placeholder="Enter your email address"></Input>
+        <Button>Join</Button>
+      </JoinContent>
     </>
   );
 };
@@ -14,5 +19,22 @@ export default Join;
 const JoinContent = styled.div`
   height: 672px;
   width: 100%;
-  background-color: purple;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: url(${joinImg}) no-repeat #ffffff right bottom;
+`;
+const JoinTitle = styled.h1``;
+const Input = styled.input`
+  width: 30%;
+`;
+const Button = styled.button`
+  background-color: #badc58;
+  border: none;
+  border-radius: 5px;
+  width: 134px;
+  color: #ffffff;
+  font-family: muli, sans-serif;
+  font-weight: 400;
+  font-style: normal;
 `;
