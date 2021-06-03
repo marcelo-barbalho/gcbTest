1bbc184b35f16c1f80f189c3a004a03ad2174289
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2021-06-03 10:57:05 -0300

    inserios componentes do blog, com estilização inicial e sem posicionamento

--- src/components/blog.js
+++ src/components/blog.js
@@ -4,7 +4,14 @@ import styled from "styled-components";
 const Blog = () => {
   return (
     <>
-      <BlogContent>Blog</BlogContent>
+      <BlogContent>
+        <BlogTitle>Read Our Blog</BlogTitle>
+        <Discription>
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts.
+        </Discription>
+        <div>entrar com carrossel aqui</div>
+      </BlogContent>
     </>
   );
 };
@@ -14,5 +21,20 @@ export default Blog;
 const BlogContent = styled.div`
   height: 668px;
   width: 100%;
-  background-color: black;
+  display: flex;
+  background-color: #fafafc;
+  justify-content: center;
+  flex-direction: column;
+`;
+const BlogTitle = styled.h1`
+  color: #1d164d;
+  font-size: 32pt;
+  font-weight: bold;
+  font-family: "Montserrat", sans-serif;
+`;
+const Discription = styled.div`
+  font-family: muli, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #9e9baf;
 `;
