551a9bdb8a7aa73cc9db9188f1371c4d810883ab
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2021-06-03 10:32:43 -0300

    inseridos textos e estilização, precisa finalizar posicionamento

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,11 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <>
-      <FooterLayout>Footer</FooterLayout>
+      <FooterLayout>
+        <Rights>© Copyrights 2019 Stack. All Rights Reserved.</Rights>
+        <Options>Privacy Policy</Options>
+        <Options>Terms and Conditions</Options>
+      </FooterLayout>
     </>
   );
 };
@@ -12,7 +16,21 @@ const Footer = () => {
 export default Footer;
 
 const FooterLayout = styled.footer`
-  background-color: red;
+  background-color: #ffffff;
   height: 103px;
   width: 100%;
+  font-family: muli, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 16pt;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`;
+const Rights = styled.div`
+  color: #1d164d;
+`;
+const Options = styled.div`
+  color: #9e9baf;
 `;
